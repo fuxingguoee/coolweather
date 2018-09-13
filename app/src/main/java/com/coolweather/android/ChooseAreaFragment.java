cbb04945d14c526c078d57155cee2ccbb0edda6f
fxg <1978640054@qq.com>
2018-09-13 13:20:40 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -77,25 +77,25 @@ public class ChooseAreaFragment extends Fragment {
                     selectedProvince = provinceList.get(position);
                     queryCities();
                 }
-//                else if (currentLevel == LEVEL_CITY) {
-//                    selectedCity = cityList.get(position);
-//                    queryCounties();
-//                } else if (currentLevel == LEVEL_COUNTY) {
-//                    //跳转到天气界面
-//                    String weatherId = countyList.get(position).getWeatherId();
-//                    if(getActivity() instanceof MainActivity) {
-//                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
-//                        intent.putExtra("weather_id", weatherId);
-//                        startActivity(intent);
-//                        getActivity().finish();
+                else if (currentLevel == LEVEL_CITY) {
+                    selectedCity = cityList.get(position);
+                    queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    //跳转到天气界面
+                    String weatherId = countyList.get(position).getWeatherId();
+                    if(getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
 //                    }else if(getActivity() instanceof WeatherActivity) {
 //                        WeatherActivity activity = (WeatherActivity) getActivity();
 //                        activity.drawerLayout.closeDrawers();
 //                        activity.swipeRefresh.setRefreshing(true);
 //                        activity.requestWeather(weatherId);
 //
-//                    }
-//                }
+                    }
+                }
             }
         });
         //设置按钮监听事件
